c80b1846d1c74b81af41aeae9c2b846872420136
Esiena Ekwofia <eekwofia@gmail.com>
2020-11-20 23:11:22 -0500

    updated psedocode

--- script.js
+++ script.js
@@ -4,35 +4,37 @@ var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
+// WHEN I click the button to generate a password
 function generatePassword(){
   var passLength= prompt("Please choose a password length between 8 and 128.")
   if (passLength >= 8  && passLength <= 128) {
     alert( `Great, your password will be ${passLength} characters long!`);
-    
+  
+  //THEN I am presented with a series of prompts for password criteria
     var passLower= confirm("Would you like to include lower case letters?");
     var passUpper= confirm("Would you like to include upper case letters?");
     var passNumeric= confirm("Would you like to include numbers in your password?");
     var passSpecial= confirm("Finally, would you like to include any special characters?");
 
-    let allChars="";
+    let theChars="";
     let password="";
 
     if (passLower) {
-      allChars += "abcdefghijklmnopqrstuvwxyz";
+      theChars += "abcdefghijklmnopqrstuvwxyz";
     }
     if (passUpper) {
-      allChars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      theChars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     }
     if (passNumeric) {
-      allChars += "1234567890";
+      theChars += "1234567890";
     }
     if (passSpecial) {
-      allChars += "!@#$%^&*()";
+      theChars += "!@#$%^&*()";
     }
   for ( i= 0; i < passLength; i++) {
-    password += allChars.charAt(Math.floor(Math.random() * allChars.length));
+    password += theChars.charAt(Math.floor(Math.random() * theChars.length));
   }
+  // THEN a password is generated that matches the selected criteria
     return password;
   } else {
     (passLength > 7 || passLength < 129)
@@ -41,7 +43,7 @@ function generatePassword(){
   
 }
 
-// Write password to the #password input
+// THEN the password is displayed 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
